76bb3648e80f3bae02ac4947485f7f684525514e
Alan Longcoy <alanlongcoy@Alans-MacBook-Pro.local>
2026-01-05 20:26:20 -0500

    fixed flags

--- src/utils/country_flags.ts
+++ src/utils/country_flags.ts
@@ -1,10 +1,10 @@
 export const countryFlags: Record<string, string> = {
-  USA: '🇺🇸',
-  Canada: '🇨🇦',
-  Brazil: '🇧🇷',
-  Australia: '🇦🇺',
-  China: '🇨🇳',
-  Israel: '🇮🇱',
-  Mexico: '🇲🇽',
-  Türkiye: '🇹🇷'
+  USA: '\u{1F1FA}\u{1F1F8}',
+  Canada: '\u{1F1E8}\u{1F1E6}',
+  Brazil: '\u{1F1E7}\u{1F1F7}',
+  Australia: '\u{1F1E6}\u{1F1FA}',
+  China: '\u{1F1E8}\u{1F1F3}',
+  Israel: '\u{1F1EE}\u{1F1F1}',
+  Mexico: '\u{1F1F2}\u{1F1FD}',
+  Türkiye: '\u{1F1F9}\u{1F1F7}'
 }
